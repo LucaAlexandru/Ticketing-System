54e1dc6b5d1d9b1b8b81b27e53b60f71e186d347
Luca Alexandru <luca_github@outlook.com>
2021-09-23 11:01:14 +0200

    Fixed bat file.
    Did not work if directory had spaces. Is now fixed.

--- run ticketing system.bat
+++ run ticketing system.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 ECHO This bat file sets the current working directory and runs the Django manage.py file which boots up the server.
-python %cd%/manage.py runserver
+python "%cd%/manage.py" runserver
 PAUSE
